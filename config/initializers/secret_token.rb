c6b0c8664a17451de8a70429ae18e0552560c272
Alex Adamo <illuzzi0nz10@gmail.com>
2016-03-03 11:36:42 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CatsApp::Application.config.secret_key_base = '77001d9abd6ea60739380cabbd7fcd675dd6ae940c0d886fdb7b369abe49415234375365675f5c98c4986a8194a92308bd3eaea82dc752467ce2dea9ad8915b7'
